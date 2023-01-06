2c4eaf70d0e7a0e22c1d79c516263ed34d18ba4d
MahesaAlghifari <114244278+MahesaAlghifari@users.noreply.github.com>
2023-01-07 00:02:44 +0700

    PBO-212310006-PRAKTIKUM-8

---
+++ PBO-212310006-PRAKTIKUM-8/src/test/java/com/ibik/pbo/praktikum/PBO_212310006_PRAKTIKUM_8/AppTest.java
@@ -0,0 +1,13 @@
+package com.ibik.pbo.praktikum.PBO_212310006_PRAKTIKUM_8;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
